56e02a37f19212ce42cd193c4b4219245394f4ba
Er sitesh chaudhary <127986547+siteshh12@users.noreply.github.com>
2025-08-31 20:33:56 +0530

    Update wsgi.py

--- blog_project/blog_project/wsgi.py
+++ blog_project/blog_project/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
-
+import os,sys
+# project path
+path = '/home/sitesh12>/Shareyourstory'
+if path not in sys.path:
+    sys.path.append(path)
+  
+# project name = folder where settings.py is  
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_project.settings')
 
 application = get_wsgi_application()
+
+
+
+
+
+
+
